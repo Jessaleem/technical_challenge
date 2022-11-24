32d5b4fbd3594246afc5095dc4c8979e361f34a7
Jessaleem <aleejess@gmail.com>
2022-11-24 17:15:22 -0500

    feat: Custom Character detail page

---
+++ components/Card.jsx
@@ -0,0 +1,29 @@
+import { useRouter } from 'next/router';
+
+const Card = ({ children, title, footer, planet }) => {
+  const router = useRouter();
+
+  return (
+    <div className='block rounded-lg shadow-lg bg-white w-96'>
+      <div className='py-3 px-6 border-b border-gray-300 text-center font-bold'>
+        {title}
+      </div>
+      <div className='p-6'>{children}</div>
+      <div className='py-3 px-6 border-t border-gray-300 text-center'>
+        {footer ? (
+          footer
+        ) : (
+          <button
+            onClick={() => router.push(`/planet/${planet}`)}
+            type='button'
+            className=' inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'
+          >
+            Go to Planet
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- pages/character/[id].js
+++ pages/character/[id].js
@@ -4,8 +4,8 @@ const CharacterDetail = ({ character, error }) => {
   if (error) return <p>Character not found!</p>;
 
   return (
-    <section className='max-w-5xl mx-auto p-8'>
-      <CharacterCard character={character} />;
+    <section className='flex justify-between max-w-5xl mx-auto p-16'>
+      <CharacterCard character={character} />
     </section>
   );
 };
